2bf29dd0b8c317320a27348d9c8828b2d4742eb5
kazzastic <rkazim79@gmail.com>
2020-05-12 03:57:18 +0500

    this script is used for converting npy into png

--- convert_to_png.py
+++ convert_to_png.py
@@ -4,10 +4,10 @@ import cv2
 import numpy  as np
 from multiprocessing import Pool 
 
-TRAIN_PATH = '/media/kazzastic/C08EBCFB8EBCEAD4/data/patches/calcification'
+TRAIN_PATH = '/media/kazzastic/C08EBCFB8EBCEAD4/data/patches/calcification/'
 #TEST_PATH = '/media/kazzastic/C08EBCFB8EBCEAD4/data/patches/calcification'
 
-NEW_TRAIN_PATH = ''
+NEW_TRAIN_PATH = '/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/'
 #NEW_TEST_PATH = '/mnt/disks/patches/calcifications/test/'
 
 def normalize(img):
@@ -27,8 +27,8 @@ def save_files(IN_PATH, img_type, OUT_PATH, file_type):
     files = os.listdir(os.path.join(IN_PATH, img_type))
     print(files[0])
     print(len(files))
-    if img_type == 'no_tumor' and file_type == 'train':
-        already_processed = os.listdir('/mnt/disks/patches/calcifications/train/no_tumor')
+    if img_type == 'no_tumor' and file_type == '':
+        already_processed = os.listdir('/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/no_tumor')
         already_processed = [i[:-4] for i in already_processed]
         files = [i[:-4] for i in files] 
         files = list(set(files) - set(already_processed))
@@ -41,15 +41,15 @@ def save_files(IN_PATH, img_type, OUT_PATH, file_type):
         path = IN_PATH + img_type + '/' + file
         try:
             img = np.load(path)
-            img = normalize(img)
+            #img = normalize(img)
         except:
             errors += 1
             continue
         
-        if np.mean(img)>10:
-            out_path = OUT_PATH + file_type +'/' +  img_type + '/'+ file[:-4] + '.png'
-            cv2.imwrite(out_path, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-            counter += 1
+        #if np.mean(img)>10:
+        out_path = OUT_PATH + file_type +'/' +  img_type + '/'+ file[:-4] + '.png'
+        cv2.imwrite(out_path, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+        counter += 1
         if counter % 1000 == 0:
             print (file)
             print("files converted: {}, total files: {}".format(counter, number))
@@ -57,13 +57,13 @@ def save_files(IN_PATH, img_type, OUT_PATH, file_type):
 
 
 if __name__ == '__main__':
-    save_files(TRAIN_PATH, 'no_tumor','/mnt/disks/patches/calcifications/', 'train')
-    save_files(TRAIN_PATH, 'benign','/mnt/disks/patches/calcifications/', 'train')
-    save_files(TRAIN_PATH, 'benign_no_callback','/mnt/disks/patches/calcifications/', 'train')
-    save_files(TRAIN_PATH, 'malignant','/mnt/disks/patches/calcifications/', 'train')
+    save_files(TRAIN_PATH, 'no_tumor','/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/', '')
+    save_files(TRAIN_PATH, 'benign','/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/', '')
+    save_files(TRAIN_PATH, 'benign_no_callback','/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/', '')
+    save_files(TRAIN_PATH, 'malignant','/media/kazzastic/C08EBCFB8EBCEAD4/data_png/patches/calcification/', '')
 
-    save_files(TEST_PATH, 'no_tumor','/mnt/disks/patches/calcifications/', 'test')
-    save_files(TEST_PATH, 'benign','/mnt/disks/patches/calcifications/', 'test')
-    save_files(TEST_PATH, 'benign_no_callback','/mnt/disks/patches/calcifications/', 'test')
-    save_files(TEST_PATH, 'malignant','/mnt/disks/patches/calcifications/', 'test')
+    #save_files(TEST_PATH, 'no_tumor','/mnt/disks/patches/calcifications/', 'test')
+    #save_files(TEST_PATH, 'benign','/mnt/disks/patches/calcifications/', 'test')
+    #save_files(TEST_PATH, 'benign_no_callback','/mnt/disks/patches/calcifications/', 'test')
+    #save_files(TEST_PATH, 'malignant','/mnt/disks/patches/calcifications/', 'test')
     
